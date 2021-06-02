a40907351de47318c4dcd1daddb5a14b1ffa4a8f
Gagan Singh <gagansingh.dev@outlook.com>
2021-06-02 17:46:12 -0400

    Changed all inputs in EventForm to controlled

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,34 +2,36 @@ import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
 const EventListItem = ({ event }) => {
+  const { hostPhotoURL, title, hostedBy, date, venue, attendees, description } =
+    event;
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+            <Item.Image size="tiny" circular src={hostPhotoURL} />
             <Item.Content>
-              <Item.Header content="Event Title" />
-              <Item.Description>Hosted by {event.hostedBy}</Item.Description>
+              <Item.Header content={title} />
+              <Item.Description>Hosted by {hostedBy}</Item.Description>
             </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> {event.date}
-          <Icon name="marker" /> {event.venue}
+          <Icon name="clock" /> {date}
+          <Icon name="marker" /> {venue}
         </span>
       </Segment>
       <Segment secondary>
         <List horizontal>
-          {event.attendees.map((attendee) => (
+          {attendees.map((attendee) => (
             <EventListAttendee key={attendee.id} attendee={attendee} />
           ))}
         </List>
       </Segment>
       <Segment clearing>
-        <div>{event.description}</div>
+        <div>{description}</div>
         <Button color="teal" floated="right" content="View Event" />
       </Segment>
     </Segment.Group>
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-const EventForm = ({ setFormOpen }) => {
+const EventForm = ({ setFormOpen, setEvents }) => {
+  const [values, setValues] = useState({
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  });
+
+  const handleFormSubmit = (e) => {
+    console.log(values);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   const formCloseHandler = (e) => {
     e.preventDefault();
     setFormOpen(false);
@@ -9,32 +28,63 @@ const EventForm = ({ setFormOpen }) => {
   return (
     <Segment clearing>
       <Header content="Create a new event" />
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Field>
-          <input type="text" placeholder="Title" />
+          <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Category" />
+          <input
+            name="category"
+            type="text"
+            placeholder="Category"
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Description" />
+          <input
+            name="description"
+            type="text"
+            placeholder="Description"
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="City" />
+          <input
+            name="city"
+            type="text"
+            placeholder="City"
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Venue" />
+          <input
+            name="venue"
+            type="text"
+            placeholder="Venue"
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="date" placeholder="Date" />
+          <input
+            name="date"
+            type="date"
+            placeholder="Date"
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Button type="submit" floated="right" positive content="Submit" />
-        <Button
-          type="submit"
-          floated="right"
-          content="Cancel"
-          onClick={formCloseHandler}
-        />
+        <Button floated="right" content="Cancel" onClick={formCloseHandler} />
       </Form>
     </Segment>
   );
