71d8d38d47886c1691caeb1e5e04e0210a754b02
Gagan Singh <gagansingh.dev@outlook.com>
2021-06-08 20:46:18 -0400

    Wired up asyncReducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,29 +1,16 @@
-// import { combineReducers } from "redux";
-// import authReducer from "../../features/auth/authReducer";
-// import eventReducer from "../../features/events/eventReducer";
-// import testReducer from "../../features/sandbox/testReducer";
-// import modalReducer from "../common/modals/modalReducer";
-
-// const rootReducer = combineReducers({
-//   test: testReducer,
-//   event: eventReducer,
-//   modals: modalReducer,
-//   auth: authReducer,
-// });
-
-// export default rootReducer;
-
 import { combineReducers } from "redux";
-import testReducer from "../../features/sandbox/testReducer";
-import eventReducer from "../../features/events/eventReducer";
-import modalReducer from "../common/modals/modalReducer";
 import authReducer from "../../features/auth/authReducer";
+import eventReducer from "../../features/events/eventReducer";
+import testReducer from "../../features/sandbox/testReducer";
+import modalReducer from "../common/modals/modalReducer";
+import asyncReducer from "./asyncReducer";
 
 const rootReducer = combineReducers({
   test: testReducer,
   event: eventReducer,
   modals: modalReducer,
   auth: authReducer,
+  async: asyncReducer,
 });
 
 export default rootReducer;
